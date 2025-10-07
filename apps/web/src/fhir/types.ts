85e0d2c3dfce574deddb22864a84b4237f26fc0c
Luis195f <luism.sotoa@gmail.com>
2025-10-07 11:46:02 +0200

    add/update apps/web/src/fhir/types.ts

---
+++ apps/web/src/fhir/types.ts
@@ -0,0 +1,7 @@
+# apps/web/src/config/flags.ts
+# -> Copia del canvas el bloque “apps/web/src/config/flags.ts”
+Write-FromClipboard apps/web/src/config/flags.ts
+
+# apps/web/src/fhir/types.ts
+Write-FromClipboard apps/web/src/fhir/types.ts
+
