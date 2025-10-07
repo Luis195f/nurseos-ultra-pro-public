31714163ee3cff73fd541363c91740243c5cf520
Luis195f <luism.sotoa@gmail.com>
2025-10-07 12:13:06 +0200

    add/update apps/web/src/pages/Farmacia.tsx

---
+++ apps/web/src/pages/Farmacia.tsx
@@ -0,0 +1,5 @@
+Write-FromClipboard apps/web/src/pages/Fisio.tsx
+Write-FromClipboard apps/web/src/pages/Matrona.tsx
+Write-FromClipboard apps/web/src/pages/Farmacia.tsx
+Write-FromClipboard apps/web/src/pages/Nutricion.tsx
+
